835fde8b708afe77cdec55f7df10bf27483d8a1a
maxnathan <maxn29@gmail.com>
2019-12-18 16:59:32 +0000

    19/12_commit

---
+++ old_site/old_v2/webfonts/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Silence is golden.</title>
+	<meta name="robots" content="noindex,nofollow">
+	<style>@import url("http://squareglasses.co.uk/squareglasses/webfonts/webfonts.css");</style>
+</head>
+<body style="width:500px;">
+	<h1 style="font-family:'FranklinGothicURW-Dem';">Silence is golden.</h1>
+	<p style="font-family:'FranklinGothicURW-Boo';">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<?php // phpinfo(); ?>
+</body>
+</html>
